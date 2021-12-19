e74858e8a0847712b5e70415e4f213ecbf584b05
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2021-12-19 20:49:29 +0300

    Added a new WriteLIne on VS 2022!

---
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -0,0 +1,4 @@
+﻿using static System.Console;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+WriteLine("Just added this on Visual Studio 2022!");
